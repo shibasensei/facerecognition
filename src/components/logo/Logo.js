dcca41c117f16b2d8fcf2e78e985380f0a5ff50a
Viacheslav Verchonv <verchonov.viacheslav@gmail.com>
2019-04-03 00:43:16 +0300

    we changed comment with Jule

--- src/components/logo/Logo.js
+++ src/components/logo/Logo.js
@@ -2,7 +2,7 @@ import React from 'react';
 import Tilt from 'react-tilt';
 import './Logo.css';
 import Brain from './brain.png';
-
+// sadsd
 const Logo = () => {
   return(
     <div className='ma4 mt0'>
